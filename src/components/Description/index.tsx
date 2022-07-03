ee51873ce268d02e70b6f8ce16c1ddc58b456430
MateusBraz <silvabraz96@gmail.com>
2022-07-03 16:39:29 -0300

    Substituindo imagem celular

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./style";
 import imgTemploIgreja from "../../assets/imagem-igreja.jpg";
-import imgCel from "../../assets/redmi-note-11.jpg";
+import imgCel from "../../assets/redmi-note-11.jpeg";
 
 const Description: React.FC = () => {
   return (
@@ -9,7 +9,7 @@ const Description: React.FC = () => {
       <h1>Rifa em prol da finalização da construção do templo da Igreja CEIAV em Aurora do Pará.</h1>
       <img style={{ width: '80%'}} src={imgTemploIgreja} alt="Templo" />
       <h1>Ajude comprando um ou mais números e concorra a um Smartphone Redmi Note 11 4GB RAM 128GB ROM Star Blue. Cada número custa apenas R$ 5,00.</h1>
-      <img style={{ width: '60%'}} src={imgCel} alt="Celular" />
+      <img style={{ width: '50%'}} src={imgCel} alt="Celular" />
       <h1>Após escolher seu número, envie para um de nossos responsáveis para fazer a reserva.</h1>
       <br />
       <strong>PROGRESSO</strong>
